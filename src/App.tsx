a40c1e7aef058377b8a258328c01f6f15e4a43d2
alonzojoe <alonzojoenell2k15@gmail.com>
2026-01-26 23:58:17 +0800

    feat: construct app content layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Navbar } from './components/layouts/navbar/navbar'
+import { Wrapper } from './components/container'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <main className="text-base text-sub antialiased">
-       <p>Content</p>
+      <main className="text-base text-sub antialiased h-dvh">
+        <section id="home" className="pt-20 lg:pt-32 bg-accent">
+          <Wrapper className='px-3 py-2 grid items-center grid-cols-1 lg:grid-cols-2 gap-8'>
+            <p className='bg-red-500'>Content</p>
+            <p className='bg-green-500'>Content</p>
+          </Wrapper>
+        </section>
       </main>
     </>
   )
