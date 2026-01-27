448b529e901971e2f32d7c518ec3c53fd3d73334
alonzojoe <alonzojoenell2k15@gmail.com>
2026-01-27 22:19:38 +0800

    feat: create logic in data extraction, create a sample mock data to be return in the jsonviewer component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
+import { useState } from 'react';
 import { Navbar } from './components/layouts/navbar/navbar'
 import { Wrapper } from './components/container'
-import { FileUpload } from './components/features'
+import { FileUpload, JsonViewer } from './components/features'
+import { Button } from './components/ui';
+import { Loader2 } from 'lucide-react';
+
+const SAMPLE_DATA = {
+        documentType: "Invoice",
+        extractedFields: {
+          invoiceNumber: "INV-2026-001",
+          date: "2026-01-27",
+          total: "$1,234.56",
+          vendor: "Agwanet",
+          items: [
+            { description: "Product A", quantity: 2, price: "$123.40" },
+            { description: "Product B", quantity: 1, price: "$234.56" }
+          ]
+        },
+      }
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [extractedData, setExtractedData] = useState<object | null>(null);
+  const [isProcessing, setIsProcessing] = useState(false);
 
-  const handleFileSelect = () => {
-    console.log('file')
-  }
+ const handleFileSelect = (file: File) => {
+    setSelectedFile(file);
+    setExtractedData(null); // Reset extracted data when new file is selected
+  };
+
+  const handleExtract = async () => {
+    if (!selectedFile) return;
+
+    setIsProcessing(true);
+    
+    // Simulating API call
+    setTimeout(() => {
+      // sample data
+      setExtractedData(SAMPLE_DATA);
+      setIsProcessing(false);
+      
+    }, 5000);
+  };
+
+  console.log('extracted', extractedData)
 
   return (
     <>
@@ -27,6 +64,24 @@ function App() {
                 </div>
 
                 <FileUpload onFileSelect={handleFileSelect} />
+
+             
+                  <Button
+                    onClick={handleExtract}
+                    disabled={isProcessing || !selectedFile}
+                    className="w-full cursor-pointer"
+                    size="lg"
+                  >
+                    {isProcessing ? (
+                      <>
+                        <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+                        Processing...
+                      </>
+                    ) : (
+                      'Extract Data'
+                    )}
+                  </Button>
+             
 
               
               </div>
